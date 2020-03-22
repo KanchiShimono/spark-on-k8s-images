c4ff0c9fab2003dd4ca05a8b274d0058461de4bf
Kanchi Shimono <dev.kanchi.shimono@gmail.com>
2020-03-23 02:55:34 +0900

    Fix pyenv installing location
    Previous pyenv is installed under the /root.
    However /root can not be accessed because of permission.

--- master/entry_point.sh
+++ master/entry_point.sh
@@ -4,8 +4,8 @@
 . "/spark/bin/load-spark-env.sh"
 
 export PYTHONPATH=$PYTHONPATH:/spark/python:/spark/python/lib/py4j-0.10.7-src.zip
-export PYSPARK_PYTHON=/root/.pyenv/shims/python3
-export PYSPARK_DRIVER_PYTHON=/root/.pyenv/shims/python3
+export PYSPARK_PYTHON=${PYENV_ROOT}/shims/python3
+export PYSPARK_DRIVER_PYTHON=${PYENV_ROOT}/shims/python3
 
 mkdir -p $SPARK_MASTER_LOG
 ln -sf /dev/stdout $SPARK_MASTER_LOG/spark-master.out
