c32f91145ce378d4b7b8030a20368642729cf824
Kanchi Shimono <dev.kanchi.shimono@gmail.com>
2020-03-23 06:18:41 +0900

    Fix bug on circleci

--- master/entry_point.sh
+++ master/entry_point.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-. "/spark/sbin/spark-config.sh"
-. "/spark/bin/load-spark-env.sh"
-
 export PYTHONPATH=$PYTHONPATH:/spark/python:/spark/python/lib/py4j-0.10.7-src.zip
 export PYSPARK_PYTHON=${PYENV_ROOT}/shims/python3
 export PYSPARK_DRIVER_PYTHON=${PYENV_ROOT}/shims/python3
@@ -13,4 +10,4 @@ ln -sf /dev/stdout $SPARK_MASTER_LOG/spark-master.out
 WORKDIR=/home/work/
 mkdir -p ${WORKDIR}
 cd ${WORKDIR}
-jupyter lab --ip=0.0.0.0 --no-browser --allow-root
+exec jupyter lab --ip=0.0.0.0 --no-browser --allow-root
