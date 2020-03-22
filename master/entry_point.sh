bf239805dcbaed16ca18b29568dccd9ad04de6ce
Kanchi Shimono <dev.kanchi.shimono@gmail.com>
2020-03-23 07:34:02 +0900

    Fix bug on circleci

--- master/entry_point.sh
+++ master/entry_point.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export PYTHONPATH=$PYTHONPATH:/spark/python:/spark/python/lib/py4j-0.10.7-src.zip
+export PYTHONPATH=$PYTHONPATH:/opt/spark/python:/opt/spark/python/lib/py4j-0.10.7-src.zip
 export PYSPARK_PYTHON=${PYENV_ROOT}/shims/python3
 export PYSPARK_DRIVER_PYTHON=${PYENV_ROOT}/shims/python3
 
