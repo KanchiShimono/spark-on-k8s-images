2e7773eac4081299a376a1dcc86e92a91206a8b5
koirand <koirand.jp@gmail.com>
2020-08-12 17:20:04 +0900

    py4jのパス修正

--- master/entry_point.sh
+++ master/entry_point.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export PYTHONPATH=$PYTHONPATH:/opt/spark/python:/opt/spark/python/lib
+export PYTHONPATH=$PYTHONPATH:/opt/spark/python:/opt/spark/python/lib/py4j-0.10.9-src.zip
 export PYSPARK_PYTHON=${PYENV_ROOT}/shims/python3
 export PYSPARK_DRIVER_PYTHON=${PYENV_ROOT}/shims/python3
 
