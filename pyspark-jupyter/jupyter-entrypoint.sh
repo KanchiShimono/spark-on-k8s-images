59268dc9855ae683ae31354c27e0502278480773
kanchi <dev.kanchi.shimono@gmail.com>
2020-09-21 00:02:22 +0900

    Change jupyter terminal to bash (#17)
    * Add jupyter config to change jupyter terminal to bash
    
    * Update to write jupyter settings in jupyter_notebook_config.py
    
    * Fix a bug
    
    * Add comment

--- pyspark-jupyter/jupyter-entrypoint.sh
+++ pyspark-jupyter/jupyter-entrypoint.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-export PYTHONPATH=$PYTHONPATH:/opt/spark/python:/opt/spark/python/lib/py4j-0.10.9-src.zip
-export PYSPARK_PYTHON=${PYENV_ROOT}/shims/python3
-export PYSPARK_DRIVER_PYTHON=${PYENV_ROOT}/shims/python3
-
 WORKDIR=/home/spark/work
 mkdir -p ${WORKDIR}
 cd ${WORKDIR}
+
 if [[ ! -z "${JUPYTER_TOKEN}" ]]; then
-    tokenOpt="--NotebookApp.token='${JUPYTER_TOKEN}'"
+    echo "c.NotebookApp.token = '${JUPYTER_TOKEN}'" >> /etc/jupyter/jupyter_notebook_config.py
 fi
-exec jupyter lab --ip=0.0.0.0 --no-browser $tokenOpt
+
+exec jupyter lab
---
+++ pyspark-jupyter/jupyter_notebook_config.py
@@ -0,0 +1,6 @@
+c.NotebookApp.ip = '0.0.0.0'
+c.NotebookApp.port = 8888
+c.NotebookApp.open_browser = False
+
+# Avoid to start terminal as login mode
+c.NotebookApp.terminado_settings = {'shell_command': ['/bin/bash']}
