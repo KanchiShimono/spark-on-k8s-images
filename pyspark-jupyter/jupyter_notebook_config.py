11387293aa3d0595dd9697086f9a6b9700a36c39
Kanchi Shimono <dev.kanchi.shimono@gmail.com>
2020-09-20 23:35:59 +0900

    Add comment

--- pyspark-jupyter/jupyter_notebook_config.py
+++ pyspark-jupyter/jupyter_notebook_config.py
@@ -2,4 +2,5 @@ c.NotebookApp.ip = '0.0.0.0'
 c.NotebookApp.port = 8888
 c.NotebookApp.open_browser = False
 
+# Avoid to start terminal as login mode
 c.NotebookApp.terminado_settings = {'shell_command': ['/bin/bash']}
