7f0e71049b5579125404b9ef0ad89e5cb6d1d380
Kanchi Shimono <dev.kanchi.shimono@gmail.com>
2020-09-20 21:43:22 +0900

    Fix a bug

--- pyspark-jupyter/jupyter-entrypoint.sh
+++ pyspark-jupyter/jupyter-entrypoint.sh
@@ -5,7 +5,7 @@ mkdir -p ${WORKDIR}
 cd ${WORKDIR}
 
 if [[ ! -z "${JUPYTER_TOKEN}" ]]; then
-    echo "c.NotebookApp.token = ${JUPYTER_TOKEN}" >> /etc/jupyter/jupyter_notebook_config.py
+    echo "c.NotebookApp.token = '${JUPYTER_TOKEN}'" >> /etc/jupyter/jupyter_notebook_config.py
 fi
 
 exec jupyter lab
