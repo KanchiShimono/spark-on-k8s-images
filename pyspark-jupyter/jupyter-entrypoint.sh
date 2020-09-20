54d0a92215b5945a293c4c3eebd4dd31421de0c4
Kanchi Shimono <dev.kanchi.shimono@gmail.com>
2020-09-20 20:28:48 +0900

    Update to write jupyter settings in jupyter_notebook_config.py

--- pyspark-jupyter/jupyter-entrypoint.sh
+++ pyspark-jupyter/jupyter-entrypoint.sh
@@ -4,14 +4,8 @@ WORKDIR=/home/spark/work
 mkdir -p ${WORKDIR}
 cd ${WORKDIR}
 
-jupyter lab --generate-config
-
-cat << EOF >> ${HOME}/.jupyter/jupyter_notebook_config.py
-c.NotebookApp.terminado_settings = { 'shell_command': ['/bin/bash'] }
-EOF
-
 if [[ ! -z "${JUPYTER_TOKEN}" ]]; then
-    echo "c.NotebookApp.token = ${JUPYTER_TOKEN}" >> ${HOME}/.jupyter/jupyter_notebook_config.py
+    echo "c.NotebookApp.token = ${JUPYTER_TOKEN}" >> /etc/jupyter/jupyter_notebook_config.py
 fi
 
-exec jupyter lab --ip=0.0.0.0 --no-browser
+exec jupyter lab
---
+++ pyspark-jupyter/jupyter_notebook_config.py
@@ -0,0 +1,5 @@
+c.NotebookApp.ip = '0.0.0.0'
+c.NotebookApp.port = 8888
+c.NotebookApp.open_browser = False
+
+c.NotebookApp.terminado_settings = {'shell_command': ['/bin/bash']}
