3186492ae95652708cd4107e0f0c4c1ae217d762
Kanchi Shimono <dev.kanchi.shimono@gmail.com>
2020-09-24 20:56:36 +0900

    Fix jupyter server setting
    1. Change to chown to chmod of jupyter_notebook_config.py
    2. Remove unnecessary token setting

--- pyspark-jupyter/jupyter-entrypoint.sh
+++ pyspark-jupyter/jupyter-entrypoint.sh
@@ -4,8 +4,4 @@ WORKDIR=/home/spark/work
 mkdir -p ${WORKDIR}
 cd ${WORKDIR}
 
-if [[ ! -z "${JUPYTER_TOKEN}" ]]; then
-    echo "c.NotebookApp.token = '${JUPYTER_TOKEN}'" >> /etc/jupyter/jupyter_notebook_config.py
-fi
-
 exec jupyter lab
