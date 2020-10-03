582c9525145639b56e691030fd2b846296ff4fca
kanchi <dev.kanchi.shimono@gmail.com>
2020-10-03 22:20:05 +0900

    Fix creating event log directory of history server (#23)

--- spark/history-server-entrypoint.sh
+++ spark/history-server-entrypoint.sh
@@ -2,10 +2,11 @@
 
 if [[ -z "${EVENT_LOG_DIR}" ]]; then
     # Default event log directory
-    EVENT_LOG_DIR="file:///tmp/spark-events"
+    EVENT_LOG_DIR="file:/tmp/spark-events"
 fi
-if [[ ! -d "${EVENT_LOG_DIR}" ]]; then
-    mkdir -p "${EVENT_LOG_DIR}"
+if [[ "${EVENT_LOG_DIR}" =~ ^file: ]] && [[ ! -e "${EVENT_LOG_DIR#file:}" ]]; then
+    # Create directory, if specified local directory (starts with file:) and path not exists
+    mkdir -p "${EVENT_LOG_DIR#file:}"
 fi
 
 export SPARK_HISTORY_OPTS="-Dspark.history.fs.logDirectory='${EVENT_LOG_DIR}'"
