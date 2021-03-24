85bdbdbb9fb5a69998e1254f544e65fc67422cec
kanchi <dev.kanchi.shimono@gmail.com>
2021-03-24 16:26:06 +0900

    Ubuntu base Spark image (#28)
    * Add custom Spark Dockerfile for building on Ubuntu
    
    * Add container image build tool
    
    * Add multi stage build for PySpark image
    
    * Disable login token
    
    * Update GitHub Actions workflow
    
    * Fix docker-image-tool.sh path in GitHub Actions
    
    * Fix syntax bug in GitHub Actions
    
    * Fix typo
    
    * Change Spark installer download location

---
+++ base-spark/README.md
@@ -0,0 +1,3 @@
+# Base Spark Image
+
+Build Ubuntu based Spark image by official tool [docker-image-tool.sh](https://github.com/apache/spark/blob/master/bin/docker-image-tool.sh).
--- pyspark-jupyter/jupyter_notebook_config.py
+++ pyspark-jupyter/jupyter_notebook_config.py
@@ -1,6 +1,7 @@
 c.NotebookApp.ip = '0.0.0.0'
 c.NotebookApp.port = 8888
 c.NotebookApp.open_browser = False
+c.NotebookApp.token = ''
 
 # Avoid to start terminal as login mode
 c.NotebookApp.terminado_settings = {'shell_command': ['/bin/bash']}
